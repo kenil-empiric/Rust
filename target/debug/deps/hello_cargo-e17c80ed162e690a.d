5f1236eb7a596d96644a42c29108790e07292994
kenil_empiric <kenil.r@empiricinfotech.com>
2024-08-27 11:08:22 +0530

    first commit

---
+++ target/debug/deps/hello_cargo-e17c80ed162e690a.d
@@ -0,0 +1,5 @@
+/home/hp/Documents/Rust programing/hello_cargo/target/debug/deps/libhello_cargo-e17c80ed162e690a.rmeta: src/main.rs
+
+/home/hp/Documents/Rust programing/hello_cargo/target/debug/deps/hello_cargo-e17c80ed162e690a.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/home/hp/Documents/Rust\ programing/hello_cargo/target/debug/hello_cargo: /home/hp/Documents/Rust\ programing/hello_cargo/src/main.rs
